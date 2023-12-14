1af31edb5a736392d34d2deb8f0149e11644f0fd
ireneseal <151920244+ireneseal@users.noreply.github.com>
2023-12-14 16:53:39 +0100

    Add files via upload

---
+++ p26B_they are coming/js/fun.js
@@ -0,0 +1,14 @@
+const MenuBtn = document.querySelector('.header-responsiveMenu');
+
+const MenuResponsive = document.querySelector('.responsiveMenu');
+
+MenuBtn.addEventListener('click', function(){
+
+    MenuResponsive.classList.add('is-active');
+
+    //alert() Siempre para comprobar que está bien capturado
+
+
+
+})
+
